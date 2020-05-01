bc6d6d8de43260e31335fe7dbfd213d7a0171788
dominik <dominik.martin2001@gmx.de>
2020-05-01 20:21:52 +0200

    1.5.2020 20:21

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-Hallo
+function getTodoItems() {
+  var items = [
+    {
+      title: "Einkaufen",
+      descriiption: "Ich muss ein paar Äpfel Kaufen",
+      color: "yellow";
+    },
+    {
+      title: "Sport",
+      description: "Heftiges Seix Pack workout",
+      color: "red";
+    },
+    {
+      title: "Hausaufgaben",
+      description: "Physik Seite 430, Nr 1a)",
+      color: "green";
+    },
+    {
+      title: "Kino",
+      description: "Mit Erich ins Kino gehen",
+      color: "blue";
+    },
+    {
+      title: "ToDo-App programmieren",
+      description: "GitHub einrichten (2h), Fake-Daten schreiben (5m)",
+      color: "green";
+    },
+    {
+      title: "Einkaufen 2",
+      description: "Einkaufen gehen, und Zeug kaufen / klauen",
+      color: "yellow";
+    },
+  ];
 
-alert("sda;")
-
-Guten Morgen 
+  return items;
+}
