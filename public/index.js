a0dac48ed6598b0bf5859784c3e7b5ec696c0c05
dominik <dominik.martin2001@gmx.de>
2020-05-16 12:32:56 +0200

    Funktioniert nur zur hälfte

--- public/index.js
+++ public/index.js
@@ -2,6 +2,7 @@ const qs = document.querySelector.bind(document);
 
 let api_url = "http://localhost:8080";
 let list_id;
+let todo_list = {};
 
 function request(url, method = "GET", data = null, headers = {}, options = {}) {
   headers = Object.assign({ "Content-Type": "application/json" }, headers);
@@ -30,70 +31,74 @@ function addTodo(title, description, color) {
 
   request(`${api_url}/t/${list_id}`, "post", new_todo).then((res) => {
     console.log(`Added todo item: ${res}`);
-    updateTodo();
+    insertTodoItem(res);
   });
 }
 
 function todoOnSubmit(event) {
-  const title = qs("#todo_input_title").value;
-  const description = qs("#todo_input_description").value;
-  const color = qs("#color_select").value;
+  const title = qs("#todo_input_title");
+  const description = qs("#todo_input_description");
+  const color = qs("#color_select");
 
-  if (!title) return false;
-
-  addTodo(title, description, color);
-
-  return false;
+  try {
+    if (!title.value) return false;
+    addTodo(title.value, description.value, color.value);
+  } finally {
+    title.value = "";
+    description.value = "";
+    color.value = "";
+    return false;
+  }
 }
 
 function todoOnDone(event) {
   event.target.classList.toggle("checked");
   const id = event.target.attributes["data-todo-id"].value;
-  const item = todo_items[id];
-  console.log(`${item} was clicked`);
+  console.log("id:", id);
+  console.log(todo_list);
+  const item = todo_list[id];
 
-  item.done = true;
-  updateTodo();
+  qs(`#todo-item-${id}`).remove();
 }
 
-function updateTodo() {
-  if (!list_id) return;
-
-  var todo_list = qs("#todo_list");
-
-  request(`${api_url}/t/${list_id}`).then(({ todo_items }) => {
-    console.log(todo_items);
-    // Schleife um Items in die Liste hinzuzufügen
-    for (i in todo_items) {
-      let item = todo_items[i];
-      console.log(item);
-      if (item.done) continue;
-
-      todo_list.insertAdjacentHTML(
-        "afterbegin",
-        // ToDo Item (HTML)
-        `<li class="todo_item">
-          <h1 class="todo_item_title">${item.title}
-            <button
-            data-todo-id="${item.id}"
-            class="todo_done_button"
-            onclick="todoOnDone(event);"></button>
-          </h1>
-          <p class="todo_item_description">${item.description}</p>
-        </li>`
-      );
-    }
-  });
+function insertTodoItem(item) {
+  qs("#todo_list").insertAdjacentHTML(
+    "afterbegin",
+    `
+    <li class="todo_item" id="todo-item-${item.id}">
+      <h1 class="todo_item_title">
+        ${item.title}
+        <button data-todo-id="${item.id}" class="todo_done_button"
+        onclick="return todoOnDone(event);"></button>
+      </h1>
+      <p class="todo_item_description">${item.description}</p>
+    </li>
+    `
+  );
 }
 
 function main() {
-  qs("#start-page").classList.remove("page-active");
-  qs("#todo-page").classList.add("page-active");
+  if (!list_id) return;
 
-  updateTodo();
+  request(`${api_url}/t/${list_id}`).then((res) => {
+    if ("error" in res) return;
+    const { todo_items } = res;
+    Object.assign(todo_list, todo_items);
+
+    // Schleife um Items in die Liste hinzuzufügen
+    for (i in todo_items) {
+      let item = todo_items[i];
+      if (item.done) continue;
+      insertTodoItem(item);
+    }
+
+    qs("#start-page").classList.remove("page-active");
+    qs("#todo-page").classList.add("page-active");
+  });
 }
 
-qs("#open_list_btn").addEventListener("click", (e) => {
+qs("#open_list_form").addEventListener("submit", (e) => {
+  // TODO: Error handling
   list_id = qs("#list_id").value;
   main();
 });
--- status_codes.js
+++ status_codes.js
@@ -1,4 +1,4 @@
 module.exports = {
-  list_not_found: { message: "Could not find a list with that id" },
-  item_not_found: { message: "Could not find a todo item with that id in the list" },
+  list_not_found: { error: "Could not find a list with that id" },
+  item_not_found: { error: "Could not find a todo item with that id in the list" },
 }
