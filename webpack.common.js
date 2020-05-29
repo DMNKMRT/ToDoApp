31a0b28ca87bcf44100ef25b2130ad6af9cd8bf2
Erich Schneider <erich.schneider97@protonmail.com>
2020-05-30 01:17:14 +0200

    webpack: remove unused import

--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const webpack = require("webpack");
 const path = require("path");
 
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
