ee64055d4a89360b331ea65e364ff8cf3028e20b
Erich Schneider <erich.schneider97@protonmail.com>
2020-05-30 10:38:22 +0200

    https-redirect-middleware: use req.secure instead of req.protocol

--- https-redirect-middleware.js
+++ https-redirect-middleware.js
@@ -1,6 +1,6 @@
 function httpsRedirectMiddleware() {
   return (req, res, next) => {
-    if (req.protocol != "https")
+    if (!req.secure)
       res.redirect("https://" + req.headers.host + req.url);
     else next();
   };
