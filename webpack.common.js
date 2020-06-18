b3274f5ddaaf256c5d94d61a3955388d7027924e
Erich Schneider <erich.schneider97@protonmail.com>
2020-06-18 19:47:22 +0200

    netlify: redirect all requests to /index.html

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 const TerserJSPlugin = require("terser-webpack-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
@@ -59,6 +60,9 @@ module.exports = (mode) => ({
       algorithm: "deflate",
       test: /\.(js|css|html|svg)$/,
       compressionOptions: { level: 9 },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ context: "public/", from: "static/*", flatten: true }],
     }),
   ],
   module: {
